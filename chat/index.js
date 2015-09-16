07aff3bd1253bd73bcb47138b841ba1c76413861
unknown <m.subhasish@hotmail.com>
2015-09-16 15:07:09 +0530

    node

---
+++ chat/index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+// Initialize appication with route / (that means root of the application)
+app.get('/',function(req,res){
+	var express=require('express');
+	app.use(express.static(path.join(__dirname)));
+	res.sendFile(path.join(__dirname,'../chat','index.html'));
+});
+
+app.get('/about',function(req,res){
+	//var express=require('express');
+//	app.use(express.static(path.join(__dirname)));
+	res.sendFile(path.join(__dirname,'../chat','index.html'));
+});
+
+
+//Register events on socket connection
+io.on('connection',function(socket){
+	socket.on('chatMessage',function(from,msg){
+		io.emit('chatMessage',from,msg);
+	});
+	socket.on('notifyUser',function(user){
+		io.emit('notifyUser',user);
+	});
+});
+// Listen application request on port 3000
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
